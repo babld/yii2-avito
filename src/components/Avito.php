00e34b62d69676d8b46fb6814d22a583a14a3143
Pavel <p.kokorin@eastinn.ru>
2025-05-24 17:53:56 +0700

    fix

--- src/components/Avito.php
+++ src/components/Avito.php
@@ -6,6 +6,7 @@ use dvizh\gallery\models\Image;
 use yii\helpers\ArrayHelper;
 use yii\helpers\StringHelper;
 use yii;
+use yii\helpers\Url;
 
 class Avito
 {
@@ -39,7 +40,7 @@ class Avito
             $return .= "\t<Images>\n";
             foreach ($model->images as $image) {
                 /** @var Image $image */
-                $return .= "\t\t" . '<Image url="' . \yii\helpers\Url::to(['/images/' . $image->filePath], true). '"/>' . "\n";
+                $return .= "\t\t" . '<Image url="' . Url::to(['/images/store/' . $image->filePath], true). '"/>' . "\n";
             }
             $return .= "\t" .'</Images>' . "\n";
             $return .= "\t" .'<VideoURL>' . $avito->video_url . '</VideoURL>' . "\n";
